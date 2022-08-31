12963720686bde9ec6199b446887c6b489e6d6ac
NV <korshunnv@mail.ru>
2022-08-31 21:09:28 +0300

    изменено название столбца, по которому берутся данные, в столбцах login и user_name записано одно и то же

--- program.py
+++ program.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 def get_table(filename, column_name):
     result = pd.read_csv(filename, delimiter=',')
-    res = result[['user_name', 'Score']]
+    res = result[['login', 'Score']]
     sort_res = res.sort_values(by='user_name')
     sort_res.rename(columns={'Score': column_name}, inplace=True)
     return sort_res
 
 
 def merge_table(table1, table2):
-    res = pd.merge(table1, table2, on='user_name', how='outer')
+    res = pd.merge(table1, table2, on='login', how='outer')
     res = res.fillna(0)
     s = res.columns[1:]
     res[s] = res[s].astype('int')
